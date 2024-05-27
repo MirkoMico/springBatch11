83a32fbd7722ff46d1f9385f670813d4f4b1b65c
Lorenzo <lorenzo.biagi2@gmail.com>
2024-05-27 15:22:07 +0200

    primo commit scheletro inail funzionante (senza security)

---
+++ MS4/src/main/java/com/example/MS4/Ms4Application.java
@@ -0,0 +1,15 @@
+package com.example.MS4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Ms4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms4Application.class, args);
+	}
+
+}
---
+++ MSX/src/test/java/com/example/MSX/MsxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MSX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
