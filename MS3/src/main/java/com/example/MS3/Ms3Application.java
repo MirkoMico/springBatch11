d058c54ea8bf02e429ca03164021c5dc81100cdc
Lorenzo <lorenzo.biagi2@gmail.com>
2024-05-31 16:54:52 +0200

    da togliere

--- MS3/src/main/java/com/example/MS3/Ms3Application.java
+++ MS3/src/main/java/com/example/MS3/Ms3Application.java
@@ -1,5 +1,6 @@
 package com.example.MS3;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -10,35 +11,42 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableBatchProcessing
+@Slf4j
 public class Ms3Application {
 
-	@Autowired
-	private JobLauncher jobLauncher;
+    @Autowired
+    private JobLauncher jobLauncher;
 
-	@Autowired
-	private Job job;
+    @Autowired
+    private Job job;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ms3Application.class, args);
+    private static final ScheduledExecutorService SCHEDULER = Executors.newScheduledThreadPool(1);
 
-	}
-	@Autowired
-	public void run() {
+    public static void main(String[] args) {
+        SpringApplication.run(Ms3Application.class, args);
 
-		try {
+    }
 
-
-			JobParameters jobParameters = new JobParametersBuilder()
-					.addLong("time", System.currentTimeMillis())
-					.toJobParameters();
-			jobLauncher.run(job, jobParameters);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-
+    @Autowired
+    public void run() {
+        SCHEDULER.scheduleAtFixedRate(() -> {
+            try {
+                JobParameters jobParameters = new JobParametersBuilder()
+                        .addLong("time", System.currentTimeMillis())
+                        .toJobParameters();
+                jobLauncher.run(job, jobParameters);
+                log.info("Schedulazione programmata di {}", job);
+            } catch (Exception err) {
+                err.printStackTrace();
+            }
+        }, 0, 20, TimeUnit.SECONDS);
+    }
 }
--- MS3/src/main/java/com/example/MS3/config/SpringBatchConfig.java
+++ MS3/src/main/java/com/example/MS3/config/SpringBatchConfig.java
@@ -3,33 +3,45 @@ package com.example.MS3.config;
 import com.example.MS3.job.EndpointItemProcessor;
 import com.example.MS3.model.Endpoint;
 import com.example.MS3.repository.EndpointRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.core.task.TaskExecutor;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 @Configuration
 @EnableBatchProcessing
+@Slf4j
 public class SpringBatchConfig {
 
     private JobBuilderFactory jobBuilderFactory;
     private StepBuilderFactory stepBuilderFactory;
     private EndpointRepository endpointRepository;
+
+    @Autowired
+    private JobLauncher jobLauncher;
+    @Autowired
+    private Job job;
+
 
     public SpringBatchConfig(JobBuilderFactory jobBuilderFactory,
                              StepBuilderFactory stepBuilderFactory,
@@ -51,8 +63,6 @@ public class SpringBatchConfig {
     }*/
 
 
-
-
     @Bean
     public FlatFileItemReader<Endpoint> reader() {
         FlatFileItemReader<Endpoint> itemReader = new FlatFileItemReader<>();
@@ -72,6 +82,7 @@ public class SpringBatchConfig {
 
         return itemReader;
     }
+
     private LineMapper<Endpoint> lineMapper() {
         DefaultLineMapper<Endpoint> lineMapper = new DefaultLineMapper<>();
 
@@ -88,10 +99,12 @@ public class SpringBatchConfig {
         return lineMapper;
 
     }
+
     @Bean
     public EndpointItemProcessor processor() {
         return new EndpointItemProcessor();
     }
+
     @Bean
     public RepositoryItemWriter<Endpoint> writer() {
         RepositoryItemWriter<Endpoint> writer = new RepositoryItemWriter<>();
@@ -100,30 +113,61 @@ public class SpringBatchConfig {
         return writer;
     }
 
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("endpoint.csv").<Endpoint, Endpoint>chunk(4)
-                .reader(reader())
-                .processor(processor())
-                .writer(writer())
-                //.taskExecutor(taskExecutor())
-                .build();
-    }
+//    @Bean
+//    public Step step1() {
+//        return stepBuilderFactory.get("endpoint.csv").<Endpoint, Endpoint>chunk(4)
+//                .reader(reader())
+//                .processor(processor())
+//                .writer(writer())
+//                //.taskExecutor(taskExecutor())
+//                .build();
+//    }
 
-    @Bean
-    public Job runJob() {
-        return jobBuilderFactory
-                .get("endpoint.csv")
-                .flow(step1())
-                .end()
-                .build();
 
-    }
+    //    @Bean
+//    public Job runJob() {
+//        log.info("Job {} salvato correttamente", jobBuilderFactory);
+//        return jobBuilderFactory
+//                .get("endpoint.csv")
+//                .flow(step1())
+//                .end()
+//                .build();
+//    }
     //@Bean
     //public TaskExecutor taskExecutor() {
     //  SimpleAsyncTaskExecutor asyncTaskExecutor = new SimpleAsyncTaskExecutor();
     //  asyncTaskExecutor.setConcurrencyLimit(10);
     //  return asyncTaskExecutor;
     //}
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("endpoint.csv").<Endpoint, Endpoint>chunk(4)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public void scheduleJobExecution() {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(() -> {
+            try {
+                JobParameters jobParameters = new JobParametersBuilder()
+                        .addLong("time", System.currentTimeMillis())
+                        .toJobParameters();
+                jobLauncher.run(job, jobParameters);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, 0, 20, TimeUnit.SECONDS);
+    }
+
+    @Bean
+    public Job runJob() {
+        return jobBuilderFactory.get("endpoint.csv")
+                .start(step1())
+                .build();
+    }
 
 }
