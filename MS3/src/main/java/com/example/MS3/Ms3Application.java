2f79bee7c9f8759c49474e5eefcefa9d9714dc9c
Lorenzo <lorenzo.biagi2@gmail.com>
2024-06-03 10:39:02 +0200

    non funziona totalmente

--- MS3/src/main/java/com/example/MS3/Ms3Application.java
+++ MS3/src/main/java/com/example/MS3/Ms3Application.java
@@ -1,5 +1,7 @@
 package com.example.MS3;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -9,36 +11,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableBatchProcessing
+@EnableScheduling
+@Slf4j
 public class Ms3Application {
 
-	@Autowired
-	private JobLauncher jobLauncher;
+    @Autowired
+    private JobLauncher jobLauncher;
 
-	@Autowired
-	private Job job;
+    @Autowired
+    private Job job;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ms3Application.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(Ms3Application.class, args);
 
-	}
-	@Autowired
-	public void run() {
+    }
 
-		try {
+    @Autowired
+    @Scheduled(fixedRate = 20000)
+    @SneakyThrows
+    public void runJob() {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("time", System.currentTimeMillis())
+                .toJobParameters();
+        jobLauncher.run(job, jobParameters);
 
-
-			JobParameters jobParameters = new JobParametersBuilder()
-					.addLong("time", System.currentTimeMillis())
-					.toJobParameters();
-			jobLauncher.run(job, jobParameters);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-
+    }
 }
--- MS3/src/main/java/com/example/MS3/config/SpringBatchConfig.java
+++ MS3/src/main/java/com/example/MS3/config/SpringBatchConfig.java
@@ -3,12 +3,14 @@ package com.example.MS3.config;
 import com.example.MS3.job.EndpointItemProcessor;
 import com.example.MS3.model.Endpoint;
 import com.example.MS3.repository.EndpointRepository;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.annotation.AfterJob;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
@@ -18,18 +20,14 @@ import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.core.task.TaskExecutor;
-
 
 @Configuration
 @EnableBatchProcessing
 public class SpringBatchConfig {
 
-    private JobBuilderFactory jobBuilderFactory;
-    private StepBuilderFactory stepBuilderFactory;
-    private EndpointRepository endpointRepository;
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EndpointRepository endpointRepository;
 
     public SpringBatchConfig(JobBuilderFactory jobBuilderFactory,
                              StepBuilderFactory stepBuilderFactory,
@@ -39,59 +37,46 @@ public class SpringBatchConfig {
         this.endpointRepository = endpointRepository;
     }
 
-
-  /*  @Bean
-    public FlatFileItemReader<Endpoint> reader() {
-        FlatFileItemReader<Endpoint> itemReader = new FlatFileItemReader<>();
-        itemReader.setResource(new FileSystemResource("spring-batch-examples-main/MS3/src/main/resources/endpoint.csv"));
-        itemReader.setName("csvReader");
-        itemReader.setLinesToSkip(1);
-        itemReader.setLineMapper(lineMapper());
-        return itemReader;
-    }*/
-
-
-
-
     @Bean
     public FlatFileItemReader<Endpoint> reader() {
         FlatFileItemReader<Endpoint> itemReader = new FlatFileItemReader<>();
         itemReader.setResource(new ClassPathResource("endpoint.csv"));
         itemReader.setName("csvReader");
         itemReader.setLinesToSkip(1);
-
         DefaultLineMapper<Endpoint> lineMapper = new DefaultLineMapper<>();
         DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
         lineTokenizer.setDelimiter(",");
         lineTokenizer.setNames("endpointId", "endpointPath", "endpointType", "endpointDate");
 
         lineMapper.setLineTokenizer(lineTokenizer);
-        lineMapper.setFieldSetMapper(new EndpointFieldSetMapper()); // Usa il nuovo mapper personalizzato
-
+        lineMapper.setFieldSetMapper(new EndpointFieldSetMapper());
         itemReader.setLineMapper(lineMapper);
 
         return itemReader;
     }
-    private LineMapper<Endpoint> lineMapper() {
-        DefaultLineMapper<Endpoint> lineMapper = new DefaultLineMapper<>();
 
-        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
-        lineTokenizer.setDelimiter(",");
-        lineTokenizer.setStrict(false);
-        lineTokenizer.setNames("endpointId", "endpointPath", "endpointType", "endpointDate");
+//    private LineMapper<Endpoint> lineMapper() {
+//        DefaultLineMapper<Endpoint> lineMapper = new DefaultLineMapper<>();
+//
+//        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+//        lineTokenizer.setDelimiter(",");
+//        lineTokenizer.setStrict(false);
+//        lineTokenizer.setNames("endpointId", "endpointPath", "endpointType", "endpointDate");
+//
+//        BeanWrapperFieldSetMapper<Endpoint> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+//        fieldSetMapper.setTargetType(Endpoint.class);
+//
+//        lineMapper.setLineTokenizer(lineTokenizer);
+//        lineMapper.setFieldSetMapper(fieldSetMapper);
+//        return lineMapper;
+//
+//    }
 
-        BeanWrapperFieldSetMapper<Endpoint> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
-        fieldSetMapper.setTargetType(Endpoint.class);
-
-        lineMapper.setLineTokenizer(lineTokenizer);
-        lineMapper.setFieldSetMapper(fieldSetMapper);
-        return lineMapper;
-
-    }
     @Bean
     public EndpointItemProcessor processor() {
         return new EndpointItemProcessor();
     }
+
     @Bean
     public RepositoryItemWriter<Endpoint> writer() {
         RepositoryItemWriter<Endpoint> writer = new RepositoryItemWriter<>();
@@ -101,29 +86,21 @@ public class SpringBatchConfig {
     }
 
     @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("endpoint.csv").<Endpoint, Endpoint>chunk(4)
+    public Step step1(){
+        return stepBuilderFactory.get("src/main/resources/endpoint.csv")
+                .<Endpoint, Endpoint>chunk(10)
                 .reader(reader())
-                .processor(processor())
                 .writer(writer())
-                //.taskExecutor(taskExecutor())
+                .allowStartIfComplete(true)
                 .build();
     }
 
     @Bean
-    public Job runJob() {
+    public Job runJob(){
         return jobBuilderFactory
                 .get("endpoint.csv")
-                .flow(step1())
-                .end()
+                .start(step1())
                 .build();
-
     }
-    //@Bean
-    //public TaskExecutor taskExecutor() {
-    //  SimpleAsyncTaskExecutor asyncTaskExecutor = new SimpleAsyncTaskExecutor();
-    //  asyncTaskExecutor.setConcurrencyLimit(10);
-    //  return asyncTaskExecutor;
-    //}
 
 }
